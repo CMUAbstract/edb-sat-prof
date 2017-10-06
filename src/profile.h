4019d56157c0d55525a4d8dab3c119a69476c73e
Alexei Colin <ac@alexeicolin.com>
2017-10-05 21:39:10 -0400

    profile: mark structs as packed

--- src/profile.h
+++ src/profile.h
@@ -8,13 +8,13 @@
 #define PROFILE_EHIST_BIN_MASK 0x0F // must match the bitfield length in event_t
 #define PROFILE_COUNT_MASK     0xFF // must match the bitfield length in event_t
 
-typedef struct {
+typedef struct __attribute__((packed)) {
     uint8_t count;
     uint8_t ehist_bin0:4;
     uint8_t ehist_bin1:4;
 } event_t;
 
-typedef struct {
+typedef struct __attribute__((packed)) {
     event_t events[NUM_EVENTS];
 } profile_t;
 #define PROFILE_SIZE sizeof(profile_t) // TODO: specify explicitly, to avoid padding
